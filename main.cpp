3310a525d1099b2718fb15d627bdaf7b8ec4e748
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-12-05 15:00:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#inlcude "lib.h"
 
 int main(){
+   char n=0; cin >> n;
+    if (tecci(n)==true) {
+    cout << n;
+    } else if (tecci(n)==false) {
+        cout << "errore";
+    }
   return 0;
 }
