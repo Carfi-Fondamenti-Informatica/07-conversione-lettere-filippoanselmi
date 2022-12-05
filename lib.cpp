db8f1bec72e6160a221c323674b4fb42d7e2c8e0
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-12-05 14:58:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+bool funzione (char &x) {
+    if (((x>=65)and(x<=90))or((x>=97)and(x<=122))) {
+        if ((x>=65)and(x<=90)) {
+            x=x+32;
+        } else if ((x>=97)and(x<=122)) {
+            x=x-32;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
