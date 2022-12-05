a99f5e2e5dda932269e22043bb3efe2f4d013575
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-12-05 15:05:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,10 @@
 #include "lib.h"
 
 bool funzione (char &x) {
-    if (((x>=65)and(x<=90))or((x>=97)and(x<=122))) {
-        if ((x>=65)and(x<=90)) {
+    if (((x>=65)&&(x<=90))||((x>=97)&&(x<=122))) {
+        if ((x>=65)&&(x<=90)) {
             x=x+32;
-        } else if ((x>=97)and(x<=122)) {
+        } else if ((x>=97)&&(x<=122)) {
             x=x-32;
         }
         return true;
